d49202ecdb66c008ffc25466df7b187085ca8bcc
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-02-08 13:36:50 +0330

    feat: implement home page tablet view

--- app/components/layout/Header.jsx
+++ app/components/layout/Header.jsx
@@ -4,16 +4,23 @@ import { Bars2Icon } from "@heroicons/react/24/outline";
 const Header = () => {
   return (
     <header className="flex items-center justify-between bg-white p-6">
-      <div className="flex items-center gap-x-2">
-        <Image
-          src="/assets/images/logo.svg"
-          alt="Logo"
-          width={25}
-          height={20}
-        />
-        <h1 className="text-xl font-black">PHOTOSNAP</h1>
+      <div className="flex items-center gap-x-8">
+        <div className="flex items-center gap-x-2">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Logo"
+            width={25}
+            height={20}
+          />
+          <h1 className="text-xl font-black">PHOTOSNAP</h1>
+        </div>
+        <ul className="hidden gap-x-8 md:flex">
+          <li className="font-bold">STORIES</li>
+          <li className="font-bold">FEATURES</li>
+          <li className="font-bold">PRICING</li>
+        </ul>
       </div>
-      <Bars2Icon className="h-6 w-6 text-black" />
+      <Bars2Icon className="h-6 w-6 text-black md:hidden" />
     </header>
   );
 };
--- app/page.js
+++ app/page.js
@@ -5,135 +5,159 @@ import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
 const Home = () => {
   return (
     <main>
-      <section className="relative h-96">
-        <Image
-          src="/assets/images/pexels-wouter-de-jong-571169.jpg"
-          alt="Photographer with sea landscape"
-          fill
-          objectFit="cover"
-        />
-      </section>
-
-      <section className="space-y-6 bg-black px-8 py-16">
-        <h1 className="text-3xl font-bold text-white">
-          CREATE AND SHARE YOUR PHOTO STORIES.
-        </h1>
-        <p className="text-base text-zinc-400">
-          Photosnap is a platform for photographers and visual storytellers. We
-          make it easy to share photos, tell stories and connect with others.
-        </p>
-        <button className="flex items-center gap-x-4 text-white">
-          GET AN INVITE <ArrowLongRightIcon className="h-6 w-6" />
-        </button>
-      </section>
-
-      <section className="relative h-96">
-        <Image
-          src="/assets/images/pexels-caio-56759.jpg"
-          alt="Office Desk"
-          fill
-          objectFit="cover"
-        />
-      </section>
-
-      <section className="space-y-6 bg-white px-8 py-16">
-        <h1 className="text-3xl font-bold text-black">
-          BEAUTIFUL STORIES EVERY TIME
-        </h1>
-        <p className="text-base text-zinc-700">
-          We provide design templates to ensure your stories look terrific.
-          Easily add photos, text, embed maps and media from other networks.
-        </p>
-        <button className="flex items-center gap-x-4 text-black">
-          VIEW THE STORIES <ArrowLongRightIcon className="h-6 w-6" />
-        </button>
-      </section>
-
-      <section className="relative h-96">
-        <Image
-          src="/assets/images/pexels-kyle-loftus-3379933.jpg"
-          alt="Photographer"
-          fill
-          objectFit="cover"
-        />
-      </section>
-
-      <section className="space-y-6 bg-white px-8 py-16">
-        <h1 className="text-3xl font-bold text-black">DESIGNED FOR EVERYONE</h1>
-        <p className="text-base text-zinc-700">
-          Photosnap can help you create stories that resonate with your
-          audience. Our tool is designed for photographers of all levels,
-          brands, businesses you name it.
-        </p>
-        <button className="flex items-center gap-x-4 text-black">
-          VIEW THE STORIES <ArrowLongRightIcon className="h-6 w-6" />
-        </button>
-      </section>
-
-      <section className="relative">
-        <div className="absolute z-10 h-96 w-full bg-black/40"></div>
-        <div className="relative h-96">
+      <section className="md:flex md:h-[600px]">
+        <div className="relative order-2 h-96 md:h-auto md:w-1/3">
           <Image
-            src="/assets/images/pexels-pixabay-417173.jpg"
-            alt="Mountain"
+            src="/assets/images/pexels-wouter-de-jong-571169.jpg"
+            alt="Photographer with sea landscape"
             fill
             objectFit="cover"
           />
         </div>
-        <div className="absolute bottom-4 left-4 right-4 z-20 text-white">
-          <h1 className="text-xl font-bold">The Mountains</h1>
-          <p>by John Appleseed</p>
-          <hr className="my-4" />
-          <button className="flex w-full items-center justify-between">
-            <span className="text-sm font-semibold">READ STORY</span>{" "}
-            <ArrowLongRightIcon className="h-6 w-6" />
+        <div className="space-y-6 bg-black px-8 py-16 md:flex md:w-2/3 md:flex-col md:justify-center">
+          <h1 className="text-3xl font-bold text-white md:text-5xl">
+            CREATE AND SHARE YOUR PHOTO STORIES.
+          </h1>
+          <p className="text-base text-zinc-400">
+            Photosnap is a platform for photographers and visual storytellers.
+            We make it easy to share photos, tell stories and connect with
+            others.
+          </p>
+          <button className="flex items-center gap-x-4 text-white">
+            GET AN INVITE <ArrowLongRightIcon className="h-6 w-6" />
           </button>
         </div>
       </section>
-
-      <section className="relative">
-        <div className="absolute z-10 h-96 w-full bg-black/40"></div>
-        <div className="relative h-96">
+      <section className="md:flex md:h-[600px]">
+        <div className="relative h-96 md:h-auto md:w-1/3">
           <Image
-            src="/assets/images/pexels-victor-wang-2415499.jpg"
-            alt="Mountain"
+            src="/assets/images/pexels-caio-56759.jpg"
+            alt="Office Desk"
             fill
             objectFit="cover"
           />
         </div>
-        <div className="absolute bottom-4 left-4 right-4 z-20 text-white">
-          <h1 className="text-xl font-bold">Sunset Cityscapes</h1>
-          <p>by Benjamin Cruz</p>
-          <hr className="my-4" />
-          <button className="flex w-full items-center justify-between">
-            <span className="text-sm font-semibold">READ STORY</span>{" "}
-            <ArrowLongRightIcon className="h-6 w-6" />
+        <div className="space-y-6 bg-white px-8 py-16 md:flex md:w-2/3 md:flex-col md:justify-center">
+          <h1 className="text-3xl font-bold text-black md:text-5xl">
+            BEAUTIFUL STORIES EVERY TIME
+          </h1>
+          <p className="text-base text-zinc-700">
+            We provide design templates to ensure your stories look terrific.
+            Easily add photos, text, embed maps and media from other networks.
+          </p>
+          <button className="flex items-center gap-x-4 text-black">
+            VIEW THE STORIES <ArrowLongRightIcon className="h-6 w-6" />
           </button>
         </div>
       </section>
-
-      <section className="relative">
-        <div className="absolute z-10 h-96 w-full bg-black/40"></div>
-        <div className="relative h-96">
+      <section className="md:flex md:h-[600px]">
+        <div className="relative order-2 h-96 md:h-auto md:w-1/3">
           <Image
-            src="/assets/images/pexels-paige-deasley-10080993.jpg"
-            alt="Mountain"
+            src="/assets/images/pexels-kyle-loftus-3379933.jpg"
+            alt="Photographer"
             fill
             objectFit="cover"
           />
         </div>
-        <div className="absolute bottom-4 left-4 right-4 z-20 text-white">
-          <h1 className="text-xl font-bold">18 Days Voyage</h1>
-          <p>by Alexei Borodin</p>
-          <hr className="my-4" />
-          <button className="flex w-full items-center justify-between">
-            <span className="text-sm font-semibold">READ STORY</span>
-            <ArrowLongRightIcon className="h-6 w-6" />
+        <div className="space-y-6 bg-white px-8 py-16 md:flex md:w-2/3 md:flex-col md:justify-center">
+          <h1 className="text-3xl font-bold text-black">
+            DESIGNED FOR EVERYONE
+          </h1>
+          <p className="text-base text-zinc-700">
+            Photosnap can help you create stories that resonate with your
+            audience. Our tool is designed for photographers of all levels,
+            brands, businesses you name it.
+          </p>
+          <button className="flex items-center gap-x-4 text-black">
+            VIEW THE STORIES <ArrowLongRightIcon className="h-6 w-6" />
           </button>
         </div>
       </section>
 
-      <section className="flex flex-col items-center space-y-6 bg-white px-8 py-16">
+      <div className="md:grid md:grid-cols-2">
+        <section className="relative">
+          <div className="absolute z-10 h-96 w-full bg-black/40"></div>
+          <div className="relative h-96">
+            <Image
+              src="/assets/images/pexels-pixabay-417173.jpg"
+              alt="Mountain"
+              fill
+              objectFit="cover"
+            />
+          </div>
+          <div className="absolute bottom-4 left-4 right-4 z-20 text-white">
+            <h1 className="text-xl font-bold">The Mountains</h1>
+            <p>by John Appleseed</p>
+            <hr className="my-4" />
+            <button className="flex w-full items-center justify-between">
+              <span className="text-sm font-semibold">READ STORY</span>{" "}
+              <ArrowLongRightIcon className="h-6 w-6" />
+            </button>
+          </div>
+        </section>
+        <section className="relative">
+          <div className="absolute z-10 h-96 w-full bg-black/40"></div>
+          <div className="relative h-96">
+            <Image
+              src="/assets/images/pexels-victor-wang-2415499.jpg"
+              alt="Mountain"
+              fill
+              objectFit="cover"
+            />
+          </div>
+          <div className="absolute bottom-4 left-4 right-4 z-20 text-white">
+            <h1 className="text-xl font-bold">Sunset Cityscapes</h1>
+            <p>by Benjamin Cruz</p>
+            <hr className="my-4" />
+            <button className="flex w-full items-center justify-between">
+              <span className="text-sm font-semibold">READ STORY</span>{" "}
+              <ArrowLongRightIcon className="h-6 w-6" />
+            </button>
+          </div>
+        </section>
+        <section className="relative">
+          <div className="absolute z-10 h-96 w-full bg-black/40"></div>
+          <div className="relative h-96">
+            <Image
+              src="/assets/images/pexels-paige-deasley-10080993.jpg"
+              alt="Mountain"
+              fill
+              objectFit="cover"
+            />
+          </div>
+          <div className="absolute bottom-4 left-4 right-4 z-20 text-white">
+            <h1 className="text-xl font-bold">18 Days Voyage</h1>
+            <p>by Alexei Borodin</p>
+            <hr className="my-4" />
+            <button className="flex w-full items-center justify-between">
+              <span className="text-sm font-semibold">READ STORY</span>
+              <ArrowLongRightIcon className="h-6 w-6" />
+            </button>
+          </div>
+        </section>
+        <section className="relative">
+          <div className="absolute z-10 h-96 w-full bg-black/40"></div>
+          <div className="relative h-96">
+            <Image
+              src="/assets/images/pexels-pixabay-417273.jpg"
+              alt="Mountain"
+              fill
+              objectFit="cover"
+            />
+          </div>
+          <div className="absolute bottom-4 left-4 right-4 z-20 text-white">
+            <h1 className="text-xl font-bold">Architectural</h1>
+            <p>by Samantha Brooks</p>
+            <hr className="my-4" />
+            <button className="flex w-full items-center justify-between">
+              <span className="text-sm font-semibold">READ STORY</span>
+              <ArrowLongRightIcon className="h-6 w-6" />
+            </button>
+          </div>
+        </section>
+      </div>
+
+      <section className="mx-auto flex max-w-lg flex-col items-center space-y-6 bg-white px-8 py-16">
         <Image
           src="/assets/images/responsive.png"
           alt="Responsive"
@@ -146,8 +170,7 @@ const Home = () => {
           responsive and stories look beautiful on any screen.
         </p>
       </section>
-
-      <section className="flex flex-col items-center space-y-6 bg-white px-8 pb-16">
+      <section className="mx-auto flex max-w-lg flex-col items-center space-y-6 bg-white px-8 pb-16">
         <Image
           src="/assets/images/infinity.png"
           alt="Responsive"
@@ -160,8 +183,7 @@ const Home = () => {
           and share all of your stories in one go.
         </p>
       </section>
-
-      <section className="flex flex-col items-center space-y-6 bg-white px-8 pb-16">
+      <section className="mx-auto flex max-w-lg flex-col items-center space-y-6 bg-white px-8 pb-16">
         <Image
           src="/assets/images/bullhorn.png"
           alt="Responsive"
